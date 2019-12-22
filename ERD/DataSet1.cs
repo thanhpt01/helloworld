056bf890184fd180b2f55d20a59f8fda2f7c2cf1
thanhddh <thanh@diadiemhay.com.vn>
2019-12-22 23:13:28 +0700

    Thêm ERD

---
+++ ERD/DataSet1.cs
@@ -0,0 +1,14 @@
+﻿namespace ERD
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
+namespace ERD {
+    
+    
+    public partial class DataSet1 {
+    }
+}
---
+++ TestBitbucketGit/TestBitbucketGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBitbucketGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestBitbucketGit
+            //test line 2
+            //line 3
+        }
+    }
+}
