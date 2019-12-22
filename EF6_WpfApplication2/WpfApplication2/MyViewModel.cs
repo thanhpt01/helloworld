56e42368fc30acb0b00da2dad000bf175ba4897e
thanhddh <thanh@diadiemhay.com.vn>
2019-12-22 23:52:48 +0700

    add EF6_WpfApplication2

---
+++ EF6_WpfApplication2/WpfApplication2/MyViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace WpfApplication2
+{
+    class MyViewModel
+    {
+        public ObservableCollection<Person> Persons { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+
+        public MyViewModel()
+        {
+            Persons = new ObservableCollection<Person>
+        {
+            new Person{Name="Eros"},
+            new Person{Name="Tethys"},
+            new Person{Name="Atlas"},
+            new Person{Name="Apollo"},
+            new Person{Name="Hades"},
+        };
+
+            DeleteCommand = new RelayCommand<Person>(
+                (p) => p != null, // CanExecute()
+                (p) => Persons.Remove(p) // Execute()
+                );
+            AddCommand = new RelayCommand<string>(
+                (s) => true, // CanExecute()
+                (s) => Persons.Add(new Person { Name = s }) // Execute()
+                );
+        }
+    }
+
+    class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ EF6_WpfApplication2/WpfApplication2/view.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.Data.Linq;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for view.xaml
+    /// </summary>
+    public partial class view : Window
+    {
+        public view()
+        {
+            InitializeComponent();
+
+            //UI = "<Label Name=\"Label1\">This is COOL!</Label>";
+            //this.Content = XamlReader.Load(UI.CreateReader());
+            DataClasses1DataContext db = new DataClasses1DataContext();
+
+            ISingleResult<ParametersSchema> abc = db.GetPARAMETERS("fs_GeneralJournal");
+            
+
+            //ISingleResult<TableSchema> abc = db.GetTableSchema("dmkh");
+
+            
+
+            var textBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+            textBlockFactory.SetValue(TextBlock.TextProperty, new Binding("PARAMETER_NAME")); // Here
+            //textBlockFactory.SetValue(TextBlock.BackgroundProperty, Brushes.Red);
+            //textBlockFactory.SetValue(TextBlock.ForegroundProperty, Brushes.Wheat);
+            textBlockFactory.SetValue(TextBlock.FontSizeProperty, 18.0);
+
+            var template = new DataTemplate();
+            template.VisualTree = textBlockFactory;
+
+            //lv.ItemTemplate = template;
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("<ListView.ItemTemplate><DataTemplate>");
+
+            //foreach (ParametersSchema p in abc)
+            //{
+            //    sb.Append("<TextBlock Text=\"{Binding " + p.PARAMETER_NAME + "}\" />");
+            //}
+
+            lv.ItemsSource = abc;
+            
+        }
+
+        public Style GetDatatemplateObject(ParametersSchema obj)
+        {
+            return null;
+        }
+    }
+
+     
+}
